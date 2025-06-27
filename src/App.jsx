136080140e0e5da9d2a00d1a9531eb0b4f10cee8
sanish <s.karki1994@gmail.com>
2025-06-27 16:53:17 +0545

    Navigate automatically to next page after certain action using useNavigate() function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Navbar from './components/Navbar';
+import Ordersummary from './components/Ordersummary';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/order-summary' element={<Ordersummary />} />
       </Routes>
     </>
   );
