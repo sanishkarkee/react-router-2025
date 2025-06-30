780f4d65a7868ea862f6ed8d90960263096efe20
sanish <s.karki1994@gmail.com>
2025-06-30 20:08:45 +0545

    Dynamic routing to send the id from one component to another component via URL, and using useParams() to get Id from the URL

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { NoMatch } from './components/NoMatch';
 import { Products } from './components/Products';
 import FeaturedProduct from './components/FeaturedProduct';
 import NewProduct from './components/NewProduct';
+import Users from './components/Users';
+import { UserDetails } from './components/UserDetails';
 
 function App() {
   return (
@@ -18,10 +20,17 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<Ordersummary />} />
+
         <Route path='products' element={<Products />}>
+          {/* INDEX ma PATH lekhu hudaina, ani ELEMENT ma chai jun component ko content show garnu xa tyo lekhne */}
+          <Route index element={<NewProduct />} />
           <Route path='featured' element={<FeaturedProduct />} />
           <Route path='new' element={<NewProduct />} />
         </Route>
+
+        <Route path='users' element={<Users />} />
+        <Route path='users/:userId' element={<UserDetails />} />
+
         <Route path='*' element={<NoMatch />} />
       </Routes>
     </>
